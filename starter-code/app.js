f63c46e2a485c515d19da5ef144794d198c57b86
Víctor Bañuls <vicbansax@gmail.com>
2019-02-09 21:30:05 +0100

    Signup and Login achieved

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,16 +2,24 @@ require('dotenv').config();
 
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
+
 const express      = require('express');
+const session      = require('express-session');
+
 const favicon      = require('serve-favicon');
 const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const User         = require("./models/user");
+
+const passport     = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt       = require('bcrypt');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/projectMap', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -44,7 +52,42 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+app.use(session({
+  secret:"our-passport",
+  resave: true,
+  saveUninitialized: true
+}))
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy((username, password, next) => {
+  User.findOne({username})
+  .then( user =>{
+      if (!user) throw new Error("Incorrect Username");
+      if (!bcrypt.compareSync(password, user.password)) throw new Error("Incorrect Password"); 
+      return next(null, user);
+  })
+  .catch(e => {
+      next(null, false, {
+          message: e.message
+      })
+  })
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
+} )
+
+passport.deserializeUser((id, cbo) => {
+  User.findById (id, (err, user) =>{
+    if (err){
+      return cb(err)
+    }
+    cbo (null, user)
+
+    })
+})
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -6,17 +6,70 @@ const User = require("../models/user")
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 
-const ensureLogin = require("connect-ensure-login");
 const mongoose     = require('mongoose');
 
-/* GET home page */
-passportRouter.get('/', (req, res, next) => {
-  res.render('index');
+const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+
+/* Home page */
+passportRouter.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-passportRouter.post("/login", passport.authenticate("local", {
+passportRouter.post("/", passport.authenticate("local", {
   successRedirect: "/profile",
   failureRedirect: "/"
 }))
 
-module.exports = router;
+/* Profile */
+passportRouter.get("/profile", (req, res, next) => {
+  res.render("profile");
+});
+
+/* Profile */
+passportRouter.get("/signup", (req,res,next) => {
+  res.render("signup");
+})
+
+passportRouter.post('/signup', (req, res, next) => {
+
+  const {
+    username,
+    password
+  } = req.body;
+
+  User.findOne({
+    username
+  })
+  .then(user => {
+    if (user !== null){
+      throw new error("Username already Exists");
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hashPass = bcrypt.hashSync(password, salt);
+
+    const newUser = new User ({
+      username,
+      password: hashPass,
+    });
+
+    return newUser.save()
+
+  })
+    .then (user => {
+      res.redirect("/profile");
+    })
+    .catch(err => {
+      res.render("/signup"), {
+        errorMessage: err.message
+      }
+    })
+}) 
+
+passportRouter.get("/logout", ensureLoggedIn("/"), (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+
+module.exports = passportRouter;
